af80524793622c6386ccc829d4db87807dc64e82
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-05-18 18:09:45 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -2,8 +2,8 @@
 #include <cs50.h>
 int main(void)
 {
-int type = get_int("Enter type of pattern ( 1 0r 2 ) ");
-int rows = get_int("Number of rows ");
+int type = get_int("Enter type of pattern ( 1 0r 2 ) /n");
+int rows = get_int("Number of rows /n");
 int i,j;
 
 

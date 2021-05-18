cd3bc556cee8ef191a3d9728b2b75e8e75635e79
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-05-18 20:16:19 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -21,7 +21,7 @@ if(type == 2){
 //program for type 1
 if(type == 1){
   for(i=1;i<=rows;i++){
-     for(j=1;j<=rows-(i-1);j++){
+     for(j=0;j<=i;j++){
         printf("#");}
           printf("\n");}}
 

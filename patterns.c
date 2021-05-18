4b72024b46aeff9e016e12c8d5762747b2aa7c26
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-05-18 20:29:15 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -6,23 +6,35 @@ int i,j;
 int type = get_int("Enter type of pattern (1 or 2) ");
 int rows = get_int("\nNumber of rows\n");
 
-  
-//program for type 2
-if(type == 2){
-  for(i=1;i<=rows;i++){
-    for(j=1;j<=rows-i;j++){
-       printf(" ");}
-         for(j=1;j<=i;j++){
-           printf("#");}
-           printf("\n");}}
-
 
 //program for type 1
-if(type == 1){
-  for(i=1;i<=rows;i++){
-     for(j=1;j<=rows-(i-1);j++){
-        printf("#");}
-          printf("\n");}}
-
+if(type == 1)
+{
+  for(i=1;i<=rows;i++)
+  {
+     for(j=1;j<=rows-(i-1);j++)
+     {
+        printf("#");
+     }
+          printf("\n");
+  }
+}  
+  
+//program for type 2
+else if(type == 2)
+{
+  for(i=1;i<=rows;i++)
+  {
+    for(j=1;j<=rows-i;j++)
+    {
+       printf(" ");
+    }
+         for(j=1;j<=i;j++)
+         {
+           printf("#");
+         }
+           printf("\n");
+  }
+}
 
 }
